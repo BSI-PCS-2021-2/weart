db0fede8ce288edf606ca42262d0de0f94ccebe0
S-ph <santiph@edu.unirio.br>
2021-12-14 19:40:42 -0300

    Adicionando Cliente e Profissional

---
+++ Projeto - WeArt/Cliente.js
@@ -0,0 +1,34 @@
+class Cliente {
+    constructor(id,login,senha) {
+        this.id = id;
+        this.login = login;
+        this.senha = senha;
+    }
+
+    email = "";
+    nome = "";
+    infoCartão = "";
+    endereçoCobrança = "";
+    telefone = "";
+    fazerLogin(){
+
+    }
+    atualizarPerfil(){
+
+    }
+    contratarServiço(){
+
+    }
+    avaliarProfissional(){
+
+    }
+    pedirRevisão(){
+
+    }
+    alterarSenha(){
+
+    }
+    deletarPerfil(){
+
+    }
+}
