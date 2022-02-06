c109040b8b6b0d17af860713410d94399bbc7c55
kevincrys <kevin.c.costa@edu.unirio.br>
2022-02-05 21:32:50 -0300

    nodemailer

--- Projeto - WeArt/routes/servico.js
+++ Projeto - WeArt/routes/servico.js
@@ -4,6 +4,8 @@ var router = express.Router();
 var path = require('path');
 var bodyParser = require('body-parser')
 const dbsql = require("../bd/dbsql");
+const fs = require("fs");
+var nodemailer = require('nodemailer');
 /* GET home page. */
 
 
@@ -32,8 +34,36 @@ router.post('/fim', function(req, res, next) {
   let date = new Date();
   dbsql.insertservico({clienteid: req.session.passport.user[0].clienteid ,profid: req.body.profid,preco: req.body.preco ,descricao: req.body.descricao,pacote: req.body.pacote, data: date})
 
+  var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'weartmails@gmail.com',
+      pass: 'tuvkkocoewdsvkwj'
+    }
+  });
+var to=req.session.passport.user[0].email;
+  var mailOptions = {
+    from: 'weartmails@gmail.com',
+    to: to,
+    subject: 'Compra efetuada',
+    text: 'sua compra foi efetuada, aguardamos seu pagamento!'
+  };
+
+  transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+  });
+
+
+
+
 
 res.redirect("http://localhost:3000/")
+
+
 
 });
 module.exports = router;
